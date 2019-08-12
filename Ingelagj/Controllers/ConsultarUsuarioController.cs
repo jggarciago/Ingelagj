1c43c0255408922e507db376590910291be09556
AlejoVictoriaT <alejovic0399@gmail.com>
2019-08-12 15:06:36 -0500

    ControladorasCrud

---
+++ Ingelagj/Controllers/ConsultarUsuarioController.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Ingelagj.Controllers
+{
+    public class ConsultarUsuarioController : ApiController
+    {
+        private Usuario ConsultarUsuario(string documento)
+        {
+            return usuario;
+        }
+
+    }
+}
