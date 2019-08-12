fbe49963f6430b5ab48ea5df9ba8bf1d0b299d1c
LauraVZuluaga <49324952+LauraVZuluaga@users.noreply.github.com>
2019-08-12 06:54:53 -0500

    Creación cuerpos de métodos controladora "Enviar Notificación"

--- Ingelagj/Controllers/EnviarNotificacionController.cs
+++ Ingelagj/Controllers/EnviarNotificacionController.cs
@@ -9,5 +9,34 @@ namespace Ingelagj.Controllers
 {
     public class EnviarNotificacionController : ApiController
     {
+        public Boolean verificarDocumento (string documento)
+        {
+            return true;
+        }
+
+        public Usuario generarUsuario (string documento)
+        {
+            return Usuario;
+        }
+
+        public string generarContraseña (Usuario usuario, string documento)
+        {
+            string contraseña = " ";
+            return contraseña;
+        }
+
+        public string generarCorreo(Usuario usuario, string documento)
+        {
+            string correo = " ";
+            return correo;
+        }
+
+        public string enviarNotificacion (Usuario usuario, string documento)
+        {
+            string notificacion = " ";
+
+            return notificacion;
+        }
+
     }
 }
