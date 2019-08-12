276ade14ff66087cb9fedf5276bcc1054b0aa888
LauraVZuluaga <49324952+LauraVZuluaga@users.noreply.github.com>
2019-08-12 08:00:44 -0500

    Documentación añadida a la clase sobre constructores.

--- Entidades/FuncionarioDesarollo.cs
+++ Entidades/FuncionarioDesarollo.cs
@@ -11,12 +11,21 @@ namespace Entidades
         string usuario;
         string contraseña;
 
+        /*
+         * Constructor clase FuncionarioDesarrollo 
+         * @param usuario usuario con el que ingresa el funcionario al sistema
+         * @contraseña es la contraseña de acceso al sistema por parte del usuario
+         */
+
         public FuncionarioDesarollo(string usuario, string contraseña)
         {
             this.Usuario = usuario;
             this.Contraseña = contraseña;
         }
 
+        /*
+         * Metodos get y set
+         */
         public string Usuario { get => usuario; set => usuario = value; }
         public string Contraseña { get => contraseña; set => contraseña = value; }
     }
--- Entidades/Usuario.cs
+++ Entidades/Usuario.cs
@@ -13,6 +13,13 @@ namespace Entidades
         string tipo;
         string correoElectronico;
 
+        /*
+         * Constructor de la clase Usuario
+         * @param nombre nombre del usuario 
+         * @param contraseña del usuario
+         * @param tipo se refiere al tipo de usuario que está ingresando al sistema (Par Evaluador, Funcionario)
+         * @param correoElectronico es el correo electronico del usuario
+         */
         public Usuario(string nombre, string contraseña, string tipo, string correoElectronico)
         {
             this.Nombre = nombre;
@@ -20,6 +27,10 @@ namespace Entidades
             this.Tipo = tipo;
             this.CorreoElectronico = correoElectronico;
         }
+
+        /*
+         * Metodos get y set
+         */
 
         public string Nombre { get => nombre; set => nombre = value; }
         public string Contraseña { get => contraseña; set => contraseña = value; }
