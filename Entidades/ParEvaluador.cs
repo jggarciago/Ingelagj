ffbda37c29e9da910f948c2b9d420cb2fbc19107
LauraVZuluaga <49324952+LauraVZuluaga@users.noreply.github.com>
2019-08-12 07:23:58 -0500

    Comentarios para la clase "Par evaluador"

--- Entidades/ParEvaluador.cs
+++ Entidades/ParEvaluador.cs
@@ -13,6 +13,15 @@ namespace Entidades
         string cedula;
         string telefono;
 
+        /*
+         * Constructor clase ParEvaluador
+         * @param nombre nombre del usuario que será el encargado de evaluar un proyecto docente
+         * @param correoElectronico es el correo del ParEvaluador
+         * @param cedula es el documento de identificacion del ParEvaluador
+         * @param telefono es el numero telefonico de contacto para el ParEvaluador 
+         */
+
+
         public ParEvaluador(string nombre, string correoElectronico, string cedula, string telefono)
         {
             this.Nombre = nombre;
@@ -20,6 +29,10 @@ namespace Entidades
             this.Cedula = cedula;
             this.Telefono = telefono;
         }
+
+        /*
+         * Metodos get y set para los atributos de la clase
+         */
 
         public string Nombre { get => nombre; set => nombre = value; }
         public string CorreoElectronico { get => correoElectronico; set => correoElectronico = value; }
