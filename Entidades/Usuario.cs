f02ac6beb1eb854a0b6bb2b8eabf95e8a544430c
LauraVZuluaga <49324952+LauraVZuluaga@users.noreply.github.com>
2019-08-12 07:10:00 -0500

    Se añaden atributos a la clase Usuario, más contructor con encapsulamiento de campos.

--- Entidades/Usuario.cs
+++ Entidades/Usuario.cs
@@ -11,6 +11,19 @@ namespace Entidades
         string nombre;
         string contraseña;
         string tipo;
+        string correoElectronico;
+
+        public Usuario(string nombre, string contraseña, string tipo, string correoElectronico)
+        {
+            this.Nombre = nombre;
+            this.Contraseña = contraseña;
+            this.Tipo = tipo;
+            this.CorreoElectronico = correoElectronico;
+        }
+
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Contraseña { get => contraseña; set => contraseña = value; }
+        public string Tipo { get => tipo; set => tipo = value; }
+        public string CorreoElectronico { get => correoElectronico; set => correoElectronico = value; }
     }
 }
-public int Usuario { get; set; }
