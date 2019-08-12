6b107300a3e975ccfbdbc578d58c7663b58a3e1d
LauraVZuluaga <49324952+LauraVZuluaga@users.noreply.github.com>
2019-08-12 07:53:34 -0500

    Documentación clase "Docente".

--- Entidades/Docente.cs
+++ Entidades/Docente.cs
@@ -11,12 +11,20 @@ namespace Entidades
         string nombre;
         string documento;
 
+        /*
+         * Constructor clase Docente 
+         *@param nombre nombre del docente
+         *@param documento numero del documento de identificacion del docente
+         */
         public Docente(string nombre, string documento)
         {
             this.Nombre = nombre;
             this.Documento = documento;
         }
 
+        /*
+         * Metodos get y set de los atributos de clase
+         */
         public string Nombre { get => nombre; set => nombre = value; }
         public string Documento { get => documento; set => documento = value; }
     }
