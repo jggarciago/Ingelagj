940f6b116247476f7e5c350872ad383242272971
jggarciago <jggarciago@gmail.com>
2019-08-22 17:47:20 -0500

    Definir Contexto

--- Entidades/Contexto.cs
+++ Entidades/Contexto.cs
@@ -8,5 +8,34 @@ namespace Entidades
 {
     public interface Contexto
     {
+        
+
+        /**
+         * Modificaciones
+         */
+        bool UpdateDocente(Docente oldDocente, Docente newDocente);
+        bool UpdateFuncionario(FuncionarioDesarollo oldFuncionarioDesarollo, FuncionarioDesarollo newFuncionarioDesarollo);
+        bool UpdateParEvaluador(ParEvaluador oldParEvaluador, ParEvaluador newParEvaluador);
+        bool UpdateProyecto(Proyecto oldProyecto, Proyecto newProyecto);
+        bool UpdateUsuario(Usuario oldUsuario, Usuario newUsuario);
+
+        /**
+         * Adiciones
+         */
+        void AddDocente(Docente newDocente);
+        void AddFuncionario(FuncionarioDesarollo newFuncionario);
+        void AddParEvaluador(ParEvaluador newParEvaluador);
+        void AddProyecto(Proyecto newProyecto);
+        void AddUsuario(Usuario newUsuario);
+        /**
+         * Eliminaciones
+         */
+        bool DeleteDocente(Docente oldDocente);
+        bool DeleteFuncionario(FuncionarioDesarollo oldFuncionarioDesarollo);
+        bool DeleteParEvaluador(ParEvaluador oldParEvaluador);
+        bool DeleteProyecto(Proyecto oldProyecto);
+        bool DeleteUsuario(Usuario oldUsuario);
+
+
     }
 }
---
+++ Entidades/ListaContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    class ListaContext : Contexto
+    {
+        public List<Docente> Docentes { get; set; }
+        public List<FuncionarioDesarollo> Funcionarios { get; set; }
+        public List<ParEvaluador> Pares { get; set; }
+        public List<Proyecto> Proyectos { get; set; }
+
+        public List<Usuario> Usuarios { get; set; }
+    }
+}
