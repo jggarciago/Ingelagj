a338608ab24d9aa4490ed4ce5e8d165dbcdcb7c8
jggarciago <jggarciago@gmail.com>
2019-08-22 18:01:43 -0500

    Implementar ListaContext

--- Entidades/DesarrolloDbContext.cs
+++ Entidades/DesarrolloDbContext.cs
@@ -19,5 +19,80 @@ namespace Persistencia
         public DbSet<Proyecto> Proyectos { get; set; }
 
         public DbSet<Usuario> Usuarios { get; set; }
+
+        public void AddDocente(Docente newDocente)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddFuncionario(FuncionarioDesarollo newFuncionario)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddParEvaluador(ParEvaluador newParEvaluador)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddProyecto(Proyecto newProyecto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddUsuario(Usuario newUsuario)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeleteDocente(Docente oldDocente)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeleteFuncionario(FuncionarioDesarollo oldFuncionarioDesarollo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeleteParEvaluador(ParEvaluador oldParEvaluador)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeleteProyecto(Proyecto oldProyecto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeleteUsuario(Usuario oldUsuario)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdateDocente(Docente oldDocente, Docente newDocente)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdateFuncionario(FuncionarioDesarollo oldFuncionarioDesarollo, FuncionarioDesarollo newFuncionarioDesarollo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdateParEvaluador(ParEvaluador oldParEvaluador, ParEvaluador newParEvaluador)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdateProyecto(Proyecto oldProyecto, Proyecto newProyecto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdateUsuario(Usuario oldUsuario, Usuario newUsuario)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- Entidades/ListaContext.cs
+++ Entidades/ListaContext.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace Entidades
 {
+    /**
+     * Clase de fachada para la base de datos.
+     */
     class ListaContext : Contexto
     {
         public List<Docente> Docentes { get; set; }
@@ -14,5 +17,114 @@ namespace Entidades
         public List<Proyecto> Proyectos { get; set; }
 
         public List<Usuario> Usuarios { get; set; }
+
+        public ListaContext()
+        {
+            Docentes = new List<Docente>();
+            Funcionarios = new List<FuncionarioDesarollo>();
+            Pares = new List<ParEvaluador>();
+            Proyectos = new List<Proyecto>();
+            Usuarios = new List<Usuario>();
+        }
+
+        public void AddDocente(Docente newDocente)
+        {
+            Docentes.Add(newDocente);
+        }
+
+        public void AddFuncionario(FuncionarioDesarollo newFuncionario)
+        {
+            Funcionarios.Add(newFuncionario);
+        }
+
+        public void AddParEvaluador(ParEvaluador newParEvaluador)
+        {
+            Pares.Add(newParEvaluador);
+        }
+
+        public void AddProyecto(Proyecto newProyecto)
+        {
+            Proyectos.Add(newProyecto);
+        }
+
+        public void AddUsuario(Usuario newUsuario)
+        {
+            Usuarios.Add(newUsuario);
+        }
+
+        public bool DeleteDocente(Docente oldDocente)
+        {
+            return Docentes.Remove(oldDocente);
+        }
+
+        public bool DeleteFuncionario(FuncionarioDesarollo oldFuncionarioDesarollo)
+        {
+            return Funcionarios.Remove(oldFuncionarioDesarollo);
+        }
+
+        public bool DeleteParEvaluador(ParEvaluador oldParEvaluador)
+        {
+            return Pares.Remove(oldParEvaluador);
+        }
+
+        public bool DeleteProyecto(Proyecto oldProyecto)
+        {
+            return Proyectos.Remove(oldProyecto);
+        }
+
+        public bool DeleteUsuario(Usuario oldUsuario)
+        {
+            return Usuarios.Remove(oldUsuario);
+        }
+
+        public bool UpdateDocente(Docente oldDocente, Docente newDocente)
+        {
+            if (DeleteDocente(oldDocente))
+            {
+                AddDocente(newDocente);
+                return true;
+            }
+            return false;
+        }
+
+        public bool UpdateFuncionario(FuncionarioDesarollo oldFuncionarioDesarollo, FuncionarioDesarollo newFuncionarioDesarollo)
+        {
+            if (DeleteFuncionario(oldFuncionarioDesarollo))
+            {
+                AddFuncionario(newFuncionarioDesarollo);
+                return true;
+            }
+            return false;
+        }
+
+        public bool UpdateParEvaluador(ParEvaluador oldParEvaluador, ParEvaluador newParEvaluador)
+        {
+            if (DeleteParEvaluador(oldParEvaluador))
+            {
+                AddParEvaluador(newParEvaluador);
+                return true;
+            }
+            return false;
+        }
+
+        public bool UpdateProyecto(Proyecto oldProyecto, Proyecto newProyecto)
+        {
+            if (DeleteProyecto(oldProyecto))
+            {
+                AddProyecto(newProyecto);
+                return true;
+            }
+            return false;
+        }
+
+        public bool UpdateUsuario(Usuario oldUsuario, Usuario newUsuario)
+        {
+            if (DeleteUsuario(oldUsuario))
+            {
+                AddUsuario(newUsuario);
+                return true;
+            }
+            return false;
+        }
     }
 }
