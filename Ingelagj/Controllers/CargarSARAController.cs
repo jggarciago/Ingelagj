d3c1da20e5002bd7251a079d29d9f7a9f9a5dc85
jggarciago <jggarciago@gmail.com>
2019-08-22 16:57:35 -0500

    Modificar Sara controller (Currently not working)

--- Ingelagj/Controllers/CargarSARAController.cs
+++ Ingelagj/Controllers/CargarSARAController.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web.Http;
 
 namespace Ingelagj.Controllers
@@ -11,8 +12,22 @@ namespace Ingelagj.Controllers
     public class CargarSARAController : ApiController
     {
         // POST api/values
-        public void Post(HttpContent file)
+        [HttpPost, Route("api/upload")]
+        public async Task<IHttpActionResult> Post()
         {
+            if (!Request.Content.IsMimeMultipartContent())
+                throw new HttpResponseException(HttpStatusCode.UnsupportedMediaType);
+
+            var provider = new MultipartMemoryStreamProvider();
+            await Request.Content.ReadAsMultipartAsync(provider);
+            foreach (var file in provider.Contents)
+            {
+                var filename = file.Headers.ContentDisposition.FileName.Trim('\"');
+                var buffer = await file.ReadAsByteArrayAsync();
+                //Todo usar archivo
+            }
+
+            return Ok();
         }
 
         private bool ValidarArchivo(HttpContent file)
