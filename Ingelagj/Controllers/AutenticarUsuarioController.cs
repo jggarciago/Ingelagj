382a542c048b9ffcc0bf20015149c2a00b05c9e1
jessicaqgomez <49324893+jessicaqgomez@users.noreply.github.com>
2019-08-22 21:14:38 -0500

    " instancia del contexto en Autenticar usuario"

---
+++ Ingelagj/Controllers/AutenticarUsuarioController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Ingelagj.Controllers
+{
+    public class AutenticarUsuarioController : ApiController
+    {
+        Contexto contexto;
+        public AutenticarUsuarioController()
+        {
+            contexto = FabricarContextos.fabricarContexto("lista");
+        }
+        public Boolean VerificarDatos(string CorreoElectronico, string Contraseña)
+        {
+            return true;
+        }
+    }
+}
+
